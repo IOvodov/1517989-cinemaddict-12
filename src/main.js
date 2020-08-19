a57c1092d760cd9cadb6c8d4aa0795deb41763ad
ovodov_ii <pain3007@mail.ru>
2020-08-20 02:13:16 +0300

    Добавил подписку на события скрытия/отображения подробной информации о фильме

--- src/main.js
+++ src/main.js
@@ -7,10 +7,11 @@ import ShowMoreButton from "./view/show-more-button.js";
 import FooterStatistics from "./view/footer-statistics.js";
 import FilmsMainSection from "./view/flims-main-section.js";
 import FilmsExtraSection from "./view/films-extra-section.js";
-import {generateFilmCard} from "./mock/film-card.js";
-import {renderElement, RenderPosition} from "./utils.js";
-import {generateUserProfile} from "./mock/user-profile.js";
-import {generateFilmsFilter} from "./mock/filter.js";
+import FilmDefails from "./view/film-details.js";
+import { generateFilmCard } from "./mock/film-card.js";
+import { renderElement, RenderPosition } from "./utils.js";
+import { generateUserProfile } from "./mock/user-profile.js";
+import { generateFilmsFilter } from "./mock/filter.js";
 
 const FILM_CARDS_COUNT = 20;
 const EXTRA_SECTION_FILMS_COUNT = 2;
@@ -19,6 +20,44 @@ const FILMS_COUNT_PER_STEP = 5;
 const filmCards = new Array(FILM_CARDS_COUNT).fill().map(generateFilmCard);
 const profile = generateUserProfile();
 const filter = generateFilmsFilter(filmCards);
+
+const renderFilmCard = (boardComponent, filmsListElement, film) => {
+  const filmCardComponent = new FilmCard(film);
+
+  const subscribeOnEvent = (selector) => {
+    filmCardComponent.element.querySelector(selector).addEventListener(`click`, () => {
+      renderFilmDetails(boardComponent, film);
+      document.body.classList.add(`hide-overflow`);
+    });
+  }
+
+  subscribeOnEvent(`.film-card__title`);
+
+  subscribeOnEvent(`.film-card__poster`);
+
+  subscribeOnEvent(`.film-card__comments`);
+
+  renderElement(filmsListElement, filmCardComponent.element, RenderPosition.BEFOREEND);
+};
+
+const renderFilmDetails = (boardComponent, film) => {
+  const filmDetailsComponent = new FilmDefails(film);
+
+  const showFilmDetails = () => {
+    boardComponent.element.appendChild(filmDetailsComponent.element);
+  };
+
+  const hideFilmDetails = () => {
+    boardComponent.element.removeChild(filmDetailsComponent.element);
+  };
+
+  filmDetailsComponent.element.querySelector(`.film-details__close-btn`).addEventListener(`click`, () => {
+    hideFilmDetails();
+    document.body.classList.remove(`hide-overflow`);
+  });
+
+  showFilmDetails();
+}
 
 const header = document.querySelector(`.header`);
 const main = document.querySelector(`.main`);
@@ -40,7 +79,7 @@ const filmsListElement = filmsMainSectionComponent.element.querySelector(`.films
 renderElement(filmsMainSectionComponent.element, filmsListElement, RenderPosition.BEFOREEND);
 
 for (let i = 0; i < Math.min(filmCards.length, FILMS_COUNT_PER_STEP); i++) {
-  renderElement(filmsListElement, new FilmCard(filmCards[i]).element, RenderPosition.BEFOREEND);
+  renderFilmCard(boardComponent, filmsListElement, filmCards[i]);
 }
 
 if (filmCards.length > FILMS_COUNT_PER_STEP) {
@@ -53,7 +92,7 @@ if (filmCards.length > FILMS_COUNT_PER_STEP) {
     e.preventDefault();
     filmCards
       .slice(renderedFilmsCount, renderedFilmsCount + FILMS_COUNT_PER_STEP)
-      .forEach((film) => renderElement(filmsListElement, new FilmCard(film).element, RenderPosition.BEFOREEND));
+      .forEach((film) => renderFilmCard(boardComponent, filmsListElement, film));
 
     renderedFilmsCount += FILMS_COUNT_PER_STEP;
 
@@ -73,7 +112,7 @@ filmsExtraSections.forEach((section) => {
   const filmsListContainer = section.querySelector(`.films-list__container`);
 
   for (let i = 0; i < EXTRA_SECTION_FILMS_COUNT; i++) {
-    renderElement(filmsListContainer, new FilmCard(filmCards[i]).element, RenderPosition.BEFOREEND);
+    renderFilmCard(boardComponent, filmsListContainer, filmCards[i]);
   }
 });
 
