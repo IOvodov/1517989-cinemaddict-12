8ace2b48b3cb3895a1eb729f7eea723a7f053de9
ovodov_ii <pain3007@mail.ru>
2020-08-23 13:43:07 +0300

    Обновил main.js

--- src/main.js
+++ src/main.js
@@ -1,111 +1,28 @@
 import UserProfile from "./view/user-profile.js";
 import Menu from "./view/site-menu.js";
 import Sort from "./view/sorting.js";
-import Board from "./view/board.js";
-import FilmCard from "./view/film-card.js";
-import ShowMoreButton from "./view/show-more-button.js";
 import FooterStatistics from "./view/footer-statistics.js";
-import FilmsMainSection from "./view/flims-main-section.js";
-import FilmsExtraSection from "./view/films-extra-section.js";
-import FilmDefails from "./view/film-details.js";
 import {generateFilmCard} from "./mock/film-card.js";
-import {renderElement, remove} from "./utils/render.js";
+import {renderElement} from "./utils/render.js";
 import {generateUserProfile} from "./mock/user-profile.js";
 import {generateFilmsFilter} from "./mock/filter.js";
+import MovieList from "./presenter/movie-list.js";
 
 const FILM_CARDS_COUNT = 20;
-const EXTRA_SECTION_FILMS_COUNT = 2;
-const FILMS_COUNT_PER_STEP = 5;
 
 const filmCards = new Array(FILM_CARDS_COUNT).fill().map(generateFilmCard);
 const profile = generateUserProfile();
 const filter = generateFilmsFilter(filmCards);
 
-const renderFilmCard = (boardComponent, filmsListElement, film) => {
-  const filmCardComponent = new FilmCard(film);
-
-  filmCardComponent.openPopupClickHandler(() => {
-    renderFilmDetails(boardComponent, film);
-    document.body.classList.add(`hide-overflow`);
-  });
-
-  renderElement(filmsListElement, filmCardComponent.element);
-};
-
-const renderFilmDetails = (boardComponent, film) => {
-  const filmDetailsComponent = new FilmDefails(film);
-
-  const showFilmDetails = () => {
-    boardComponent.element.appendChild(filmDetailsComponent.element);
-  };
-
-  const hideFilmDetails = () => {
-    boardComponent.element.removeChild(filmDetailsComponent.element);
-  };
-
-  filmDetailsComponent.closePopupClickHandler(() => {
-    hideFilmDetails();
-    document.body.classList.remove(`hide-overflow`);
-  });
-
-  showFilmDetails();
-};
-
 const header = document.querySelector(`.header`);
 const main = document.querySelector(`.main`);
+const footer = document.querySelector(`.footer`);
 
 renderElement(header, new UserProfile(profile));
 renderElement(main, new Menu(filter));
 renderElement(main, new Sort());
 
-const boardComponent = new Board();
-
-renderElement(main, boardComponent);
-
-const filmsMainSectionComponent = new FilmsMainSection();
-
-renderElement(boardComponent, filmsMainSectionComponent);
-
-const filmsListElement = filmsMainSectionComponent.element.querySelector(`.films-list__container`);
-
-renderElement(filmsMainSectionComponent, filmsListElement);
-
-for (let i = 0; i < Math.min(filmCards.length, FILMS_COUNT_PER_STEP); i++) {
-  renderFilmCard(boardComponent, filmsListElement, filmCards[i]);
-}
-
-if (filmCards.length > FILMS_COUNT_PER_STEP) {
-  let renderedFilmsCount = FILMS_COUNT_PER_STEP;
-  const showMoreButtonComponent = new ShowMoreButton();
-
-  renderElement(filmsMainSectionComponent, showMoreButtonComponent);
-
-  showMoreButtonComponent.showMoreClickHandler(() => {
-    filmCards
-      .slice(renderedFilmsCount, renderedFilmsCount + FILMS_COUNT_PER_STEP)
-      .forEach((film) => renderFilmCard(boardComponent, filmsListElement, film));
-
-    renderedFilmsCount += FILMS_COUNT_PER_STEP;
-
-    if (renderedFilmsCount >= filmCards.length) {
-      remove(showMoreButtonComponent);
-    }
-  });
-}
-
-renderElement(boardComponent, new FilmsExtraSection(`Top rated`));
-renderElement(boardComponent, new FilmsExtraSection(`Most commented`));
-
-const filmsExtraSections = boardComponent.element.querySelectorAll(`.films-list--extra`);
-
-filmsExtraSections.forEach((section) => {
-  const filmsListContainer = section.querySelector(`.films-list__container`);
-
-  for (let i = 0; i < EXTRA_SECTION_FILMS_COUNT; i++) {
-    renderFilmCard(boardComponent, filmsListContainer, filmCards[i]);
-  }
-});
-
-const footer = document.querySelector(`.footer`);
+const movieListPresenter = new MovieList(main);
+movieListPresenter.init(filmCards);
 
 renderElement(footer, new FooterStatistics(filmCards.length));
