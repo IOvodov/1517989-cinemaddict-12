2a48d738c35b5551afcbf3f75ff73c6f055c1716
ovodov_ii <pain3007@mail.ru>
2020-09-20 17:16:26 +0300

    feat(model): add new films model methods

--- src/model/films.js
+++ src/model/films.js
@@ -7,11 +7,29 @@ export default class FilmsModel extends Observer {
     this._films = [];
   }
 
-  set films(filmsList) {
+  set films(updateType, filmsList) {
     this._films = filmsList.slice();
+
+    this._notifyListeners(updateType);
   }
 
   get films() {
     return this._films;
   }
+
+  updateFilmCard = (updateType, update) => {
+    const index = this._films.findIndex((film) => film.id === update.id);
+
+    if (index === -1) {
+      throw new Error(`Can't update unexisting film card`);
+    }
+
+    this._films = [
+      ...this._films.slice(0, index),
+      update,
+      ...this._films.slice(index + 1)
+    ];
+
+    this._notifyListeners(updateType, update);
+  };
 }
