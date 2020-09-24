b84cf97c0880667ec1a12d3b868895619cdee024
ovodov_ii <pain3007@mail.ru>
2020-09-24 07:42:40 +0300

    fix(mock): change filter name

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -1,5 +1,5 @@
 const filmToFilterMap = {
-  watchList: (films) => films.filter((film) => film.isWatchList).length,
+  watchlist: (films) => films.filter((film) => film.isWatchList).length,
   history: (films) => films.filter((film) => film.isWatched).length,
   favorites: (films) => films.filter((film) => film.isFavorite).length
 };
