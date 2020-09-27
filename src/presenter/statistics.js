9c8b197454c937a152dbcdcf05c3affd0789237f
ovodov_ii <pain3007@mail.ru>
2020-09-27 13:18:00 +0300

    feat(presenter): add statistic presenter

---
+++ src/presenter/statistics.js
@@ -0,0 +1,47 @@
+import FilterModel from '../model/filter.js';
+import StatisticView from '../view/statistic.js';
+import {UpdateType} from '../const.js';
+
+export default class StatisticPresenter {
+  constructor(statisticsContainer, data) {
+    this._data = data;
+    this._currentFilter = null;
+    this._statisticsContainer = statisticsContainer;
+
+    this._filterModel = new FilterModel();
+
+    this._handleFilterTypeChange = this._handleFilterTypeChange.bind(this);
+    this._handleModelEvent = this._handleModelEvent.bind(this);
+
+    this._filterModel.addObserver(this._handleModelEvent);
+  }
+
+  init() {
+    this._currentFilter = this._filterModel.getFilter();
+    this._staticticsComponent = new StatisticView(this._data, this._currentFilter);
+    this._staticticsComponent.setFilterTypeChangeHandler(this._handleFilterTypeChange);
+
+    renderElement(this._statisticsContainer, this._staticticsComponent);
+  }
+
+  destroy() {
+    remove(this._staticticsComponent);
+  }
+
+  _handleFilterTypeChange(filterType) {
+    if (this._currentFilter === filterType) {
+      return;
+    }
+
+    this._filterModel.setFilter(UpdateType.MAJOR, filterType);
+  }
+
+  _handleModelEvent(updateType) {
+    switch (updateType) {
+      case UpdateType.MAJOR:
+        this.destroy();
+        this.init();
+        break;
+    }
+  }
+}
